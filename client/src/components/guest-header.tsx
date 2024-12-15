46d6b52a79dc4ba87a49c2410a20571fdfed41de
Jeet Gangwar <jeetgangwar625@gmail.com>
2024-12-15 11:52:04 +0530

    Remove commented-out code for GoBackButton in GuestHeader component

--- client/src/components/guest-header.tsx
+++ client/src/components/guest-header.tsx
@@ -12,12 +12,6 @@ export const GuestHeader = ({ title, description }: GuestHeaderProps) => {
   return (
     <div className="relative space-y-4 bg-gradient-primary px-4 py-2 pb-8 text-clr-neutral">
       <header className="fixed left-1/2 top-0 z-[999] flex w-full max-w-[26rem] -translate-x-1/2 bg-gradient-primary py-1">
-        {/* <Link
-          to="/"
-          className="rounded-full p-1 hover:bg-clr-neutral/20"
-        >
-          <ChevronLeft className="h-6 text-clr-neutral" />
-        </Link> */}
         <GoBackButton className="absolute left-0 top-1/2 -translate-y-1/2" />
         <Link to="/" className="mx-auto">
           <ApplicationLogo className="h-8 w-8 fill-current" />
